dd96193ea9e160152b8b52f1390eb6ae9e9e641d
Wadim <Wadim.Bader@Student.HTW-Berlin.de>
2021-04-14 15:03:00 +0200

    (2.ersterRoterTest)

--- app/src/test/java/prog2/ha1/testing/CalculatorTest.java
+++ app/src/test/java/prog2/ha1/testing/CalculatorTest.java
@@ -52,5 +52,20 @@ class CalculatorTest {
 
         assertEquals(expected, actual);
     }
-}
 
+    @Test
+    @DisplayName("should display result after modulo two numbers")
+    void testModulo() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(6);
+        calc.pressBinaryOperationKey("%");
+        calc.pressDigitKey(2);
+        calc.pressEqualsKey();
+
+        String expected = "0";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+}
