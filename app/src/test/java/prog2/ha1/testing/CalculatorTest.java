0b040b4bc4415c1060d575ace9c2e75c1fcba398
Adrian Franz <adrianfranz@Adrians-MacBook-Air.local>
2021-04-16 09:00:03 +0200

    Commit 2 - 1. Test

--- app/src/test/java/prog2/ha1/testing/CalculatorTest.java
+++ app/src/test/java/prog2/ha1/testing/CalculatorTest.java
@@ -56,5 +56,23 @@ class CalculatorTest {
         assertEquals(expected, actual);
 
     }
+
+    @Test
+    @DisplayName("should display result after adding two negative numbers")
+    void testNegativeAddition() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(-1);
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(-2);
+        calc.pressEqualsKey();
+
+        String expected = "-3";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+
+    }
+
 }
 
