bd1085a5c0985a9717b89e68b1decaab918e77b6
gflachs <76994743+gflachs@users.noreply.github.com>
2021-04-17 14:07:34 +0200

    Found one Red Test

--- app/src/test/java/prog2/ha1/testing/CalculatorTest.java
+++ app/src/test/java/prog2/ha1/testing/CalculatorTest.java
@@ -45,6 +45,20 @@ class CalculatorTest {
         assertThrows(IllegalArgumentException.class,()->calc.pressDigitKey(14));
     }
 
+    @Test
+    @DisplayName("")
+    void testMultipleOperations(){
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(5);
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(5);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(7);
+        calc.pressEqualsKey();
+        assertEquals("3", calc.readScreen());
+    }
+
     //TODO hier weitere Tests erstellen
 }
 
