722a657fce99b96ae78e63b32024fe8b5f3d0c74
ilia <ilyasokolovskij@gmail.com>
2021-04-19 19:01:41 +0200

    3.Teilaufgabe(2) Test wird grün! (BugFix der Wurzel-Taste) [Commit Nr.5]

--- app/src/main/java/prog2/ha1/testing/Calculator.java
+++ app/src/main/java/prog2/ha1/testing/Calculator.java
@@ -38,6 +38,15 @@ public class Calculator {
 
     public void pressUnaryOperationKey(String operation) { // also die Tasten Wurzel, %, 1/x
 
+        var result = switch (operation) {
+            case "sqrt" -> Math.sqrt(Double.parseDouble(screen));
+            case "%" -> Double.parseDouble(screen) / 100;
+            case "1/x" -> 1 / Double.parseDouble(screen);
+            default -> throw new IllegalArgumentException();
+        };
+
+        screen = Double.toString(result);
+        if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
     }
 
     public void pressDotKey() { // die Komma- bzw. Punkt-Taste
