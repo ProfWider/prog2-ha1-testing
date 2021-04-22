123b302c5c9ba7b58dc96440d7284ba5632412d4
Just-T <tobias_armbruster@hotmail.de>
2021-04-22 14:44:09 +0200

    can Add digits after a dot made red Test green

--- app/src/main/java/prog2/ha1/testing/Calculator.java
+++ app/src/main/java/prog2/ha1/testing/Calculator.java
@@ -18,12 +18,13 @@ public class Calculator {
 
         if(screen.equals("0")) screen = "";
 
-        if(latestOperation.isEmpty()) {
+        if(latestOperation.isEmpty() || screen.endsWith(".")) {
             screen = screen + digit;
         } else {
             latestValue = Double.parseDouble(screen);
             screen = Integer.toString(digit);
         }
+
     }
 
     public void pressClearKey() { // die Taste CE
