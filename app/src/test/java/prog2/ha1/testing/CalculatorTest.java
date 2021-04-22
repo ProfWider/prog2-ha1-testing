d4f3b7a2effe8732b731946290e514f4def35306
Nick Schmeiter <nickschmeiter98@gmail.com>
2021-04-22 18:07:11 +0200

    neuer grüner Test

--- app/src/test/java/prog2/ha1/testing/CalculatorTest.java
+++ app/src/test/java/prog2/ha1/testing/CalculatorTest.java
@@ -36,7 +36,21 @@ class CalculatorTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    @DisplayName("should display result after adding two positive numbers")
+    void testnegativeAddition() {
+        Calculator calc = new Calculator();
 
+        calc.pressDigitKey(2);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(2);
+        calc.pressEqualsKey();
+
+        String expected = "0";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
     //TODO hier weitere Tests erstellen
 }
 
