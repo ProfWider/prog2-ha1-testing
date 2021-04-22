9df5d7dccb90c967eaced2ea282bd52ed826e82e
Lauramuecke <laura1.muecke@gmail.com>
2021-04-22 18:39:41 +0200

    3. Commit für den Fix zu diesem Test

--- app/src/main/java/prog2/ha1/testing/Calculator.java
+++ app/src/main/java/prog2/ha1/testing/Calculator.java
@@ -38,6 +38,13 @@ public class Calculator {
 
     public void pressUnaryOperationKey(String operation) { // also die Tasten Wurzel, %, 1/x
 
+        var result = switch(operation) {
+            case "1/x" -> 1 / Double.parseDouble(screen);
+            default -> throw new IllegalArgumentException();
+        };
+
+        screen = Double.toString(result);
+
     }
 
     public void pressDotKey() { // die Komma- bzw. Punkt-Taste
