aff4efbb33af516eefbe467839b81f0f80c56862
Nick Schmeiter <nickschmeiter98@gmail.com>
2021-04-22 18:25:25 +0200

    fix vom ersten roten test

--- app/src/main/java/prog2/ha1/testing/Calculator.java
+++ app/src/main/java/prog2/ha1/testing/Calculator.java
@@ -33,7 +33,9 @@ public class Calculator {
     }
 
     public void pressBinaryOperationKey(String operation)  { // also die Tasten /,x,-,+
+
         latestOperation = operation;
+
     }
 
     public void pressUnaryOperationKey(String operation) { // also die Tasten Wurzel, %, 1/x
@@ -58,5 +60,8 @@ public class Calculator {
         };
         screen = Double.toString(result);
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
+        if(screen == "Infinity"){
+            screen="error";
+        }
     }
 }
