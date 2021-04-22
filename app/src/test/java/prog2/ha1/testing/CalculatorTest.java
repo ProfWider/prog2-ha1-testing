591f12fece5214b1850cca92c8e63721b78e21cf
Yafaye Korres <s0575627@htw-berlin.de>
2021-04-22 21:36:50 +0200

    Add substraction unit test

--- app/src/test/java/prog2/ha1/testing/CalculatorTest.java
+++ app/src/test/java/prog2/ha1/testing/CalculatorTest.java
@@ -38,5 +38,22 @@ class CalculatorTest {
     }
 
     //TODO hier weitere Tests erstellen
+
+    @Test
+    @DisplayName("should display result after substracting two numbers")
+    void testSubstraction() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(9);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(5);
+        calc.pressEqualsKey();
+
+        String expected = "4";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
 }
 
