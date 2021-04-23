70c510c48b76ac7955c1e4007b5d0b19d11d501f
JuliaBrening <76996469+JuliaBrening@users.noreply.github.com>
2021-04-23 16:28:20 +0200

    Teilaufgabe 1: grüner Test (s0579035)

--- app/src/test/java/prog2/ha1/testing/CalculatorTest.java
+++ app/src/test/java/prog2/ha1/testing/CalculatorTest.java
@@ -56,34 +56,4 @@ class CalculatorTest {
 
         assertEquals(expected, actual);
     }
-    /**
-     * HA Teilaufgabe 2: Schreiben Sie zwei weitere zusätzliche Tests, die zwei unterschiedliche Implementierungslücken bzw. Fehler aufdecken (und in jedem Fall zwei unterschiedliche Methoden-Aufruf-Kombinationen testen) und somit fehlschlagen.
-     */
-    @Test
-    @DisplayName("should display negative number")
-    void testNegativeNumber() {
-        Calculator calc = new Calculator();
-
-        calc.pressNegativeKey();
-        calc.pressDigitKey(2);
-
-        String expected = "-2";
-        String actual = calc.readScreen();
-
-        assertEquals(expected, actual);
-    }
-    @Test
-    @DisplayName("should display percentage in decimal number")
-    void testPercentButton() {
-        Calculator calc = new Calculator();
-
-        calc.pressDigitKey(7);
-        calc.pressDigitKey(5);
-        calc.pressUnaryOperationKey("%");
-
-        String expected = "0.75";
-        String actual = calc.readScreen();
-
-        assertEquals(expected, actual);
-    }
 }
