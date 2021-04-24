3cb6110c21076392ef025c8136ab1db1e6ca02bb
NPhilip576065 <Philip.Nguyen@Student.HTW-Berlin.de>
2021-04-24 17:46:48 +0200

    first green commit

--- app/src/test/java/prog2/ha1/testing/CalculatorTest.java
+++ app/src/test/java/prog2/ha1/testing/CalculatorTest.java
@@ -8,35 +8,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
-    @DisplayName("should display correct number after pressing digit keys")
-    void testDigitInput() {
+    @DisplayName("should display result after multiplying two positive numbers")
+    void calculatorCanDoTwoTimesFour()
+    {
         Calculator calc = new Calculator();
-
+        calc.pressDigitKey(2);
+        calc.pressBinaryOperationKey("x");
         calc.pressDigitKey(4);
-        calc.pressDigitKey(2);
-
-        String expected = "42";
-        String actual = calc.readScreen();
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("should display result after adding two positive numbers")
-    void testPositiveAddition() {
-        Calculator calc = new Calculator();
-
-        calc.pressDigitKey(2);
-        calc.pressBinaryOperationKey("+");
-        calc.pressDigitKey(2);
         calc.pressEqualsKey();
-
-        String expected = "4";
-        String actual = calc.readScreen();
-
-        assertEquals(expected, actual);
+        assertEquals("8", calc.readScreen());
+        System.out.println(calc.readScreen());
     }
 
-    //TODO hier weitere Tests erstellen
 }
 
