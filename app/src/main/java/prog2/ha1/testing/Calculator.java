126819f7bf93ec6ce5905e1558ddebefde0bc35d
Fiona Rose Fischer <fiona.fischer@student.htw-berlin.de>
2021-04-24 14:25:24 +0200

    fixed first red test

--- app/src/main/java/prog2/ha1/testing/Calculator.java
+++ app/src/main/java/prog2/ha1/testing/Calculator.java
@@ -56,7 +56,12 @@ public class Calculator {
             case "/" -> latestValue / Double.parseDouble(screen);
             default -> throw new IllegalArgumentException();
         };
-        screen = Double.toString(result);
-        if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
+        if (Double.isInfinite(result) && latestOperation.equals("/")) { //
+            screen = "Error"; //
+
+        } else {
+            screen = Double.toString(result);
+            if (screen.endsWith(".0")) screen = screen.substring(0, screen.length() - 2);
+        }
     }
 }
