d32136782874660f935744bde25b8112ce6eb094
is_li <i.s_linda@yahoo.com>
2021-04-24 20:44:31 +0200

    Fix zu dem zweiten roten Test

--- app/src/main/java/prog2/ha1/testing/Calculator.java
+++ app/src/main/java/prog2/ha1/testing/Calculator.java
@@ -37,6 +37,14 @@ public class Calculator {
     }
 
     public void pressUnaryOperationKey(String operation) { // also die Tasten Wurzel, %, 1/x
+        latestOperation = operation;
+        var result = switch(latestOperation) {
+            case "%" -> Double.parseDouble(screen) / 100;
+            case "1/x" -> 1/ Double.parseDouble(screen);
+            default -> throw new IllegalArgumentException();
+        };
+        screen = Double.toString(result);
+        if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
 
     }
 
--- app/src/test/java/prog2/ha1/testing/CalculatorTest.java
+++ app/src/test/java/prog2/ha1/testing/CalculatorTest.java
@@ -114,5 +114,6 @@ class CalculatorTest {
     }
 
 
+
 }
 
