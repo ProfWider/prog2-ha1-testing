931c837bd8a1e1e51192a70b21fcebd37356da22
Cem Turan <Cem.Turan@Student.HTW-Berlin.de>
2021-04-25 14:28:34 +0200

    First red Test.

--- app/src/test/java/prog2/ha1/testing/CalculatorTest.java
+++ app/src/test/java/prog2/ha1/testing/CalculatorTest.java
@@ -54,4 +54,19 @@ class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    @DisplayName("should display ERROR after you press equals")
+    void testZeroDivision() {
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(4);
+        calc.pressBinaryOperationKey("/");
+        calc.pressDigitKey(0);
+        calc.pressEqualsKey();
+
+        String expected = "ERROR";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
 }
