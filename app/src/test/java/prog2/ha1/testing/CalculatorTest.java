a124f3f2a871d45ce3c7bfc443e30cac559438ac
Mo-Jamos <mohammad@DESKTOP-KAL21OB.localdomain>
2021-04-25 16:43:08 +0200

    first red test von cal1

--- app/src/test/java/prog2/ha1/testing/CalculatorTest.java
+++ app/src/test/java/prog2/ha1/testing/CalculatorTest.java
@@ -38,5 +38,38 @@ class CalculatorTest {
     }
 
     //TODO hier weitere Tests erstellen
+    @Test
+    @DisplayName("Diminution zwischen zwei_positive Zahlen")
+    void testPositivesubtraction() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(6);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(2);
+        calc.pressEqualsKey();
+
+        String expected = "4.0";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("should display correct number after pressing digit keys")
+    void testPlusWithDivision() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(6);
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(2);
+        calc.pressBinaryOperationKey("/");
+        calc.pressDigitKey(12);
+        calc.pressEqualsKey();
+
+        String expected = "0.16666666666666666";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
 }
 
