79d36662c7baaf5ecb2dddd40e67daf69534d5e4
Antonio Drewitz <Antonio.Drewitz@Student.HTW-Berlin.de>
2021-04-25 13:57:04 +0200

    Fix für percentOperation Test

--- app/src/main/java/prog2/ha1/testing/Calculator.java
+++ app/src/main/java/prog2/ha1/testing/Calculator.java
@@ -38,6 +38,12 @@ public class Calculator {
 
     public void pressUnaryOperationKey(String operation) { // also die Tasten Wurzel, %, 1/x
 
+        var result = switch (operation) {
+            case "%" -> 0.01 * Double.parseDouble(screen);
+            default -> throw new IllegalArgumentException();
+        };
+        screen = Double.toString(result);
+        if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
     }
     public void pressDotKey() { // die Komma- bzw. Punkt-Taste
         if(!screen.endsWith(".")) screen = screen + ".";
