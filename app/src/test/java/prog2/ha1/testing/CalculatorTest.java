db6397a0d8a6218a70a3e0c1b7091af9db993024
Al-Harethi <Ahmed.al-harethi@student.htw-berlin.de>
2021-04-25 15:06:13 +0200

    neuer grüner Test: neuer zusaetzlicher Test,der eine bisher nicht getestete Funktionalitaet abdeckt

--- app/src/test/java/prog2/ha1/testing/CalculatorTest.java
+++ app/src/test/java/prog2/ha1/testing/CalculatorTest.java
@@ -45,38 +45,13 @@ class CalculatorTest {
         Calculator calc = new Calculator();
         calc.pressDigitKey(2);
         calc.pressBinaryOperationKey("x");
-        calc.pressNegativeKey();
         calc.pressDigitKey(2);
         calc.pressEqualsKey();
         calc.pressClearKey();
         assertEquals("0",calc.readScreen());
     }
 
-    @Test
-    @DisplayName("should display result after multiplying two numbers, one of them is negative")
-    void calculatorCanDoTwoMultiplyMinusTwo() {
-        Calculator calc = new Calculator();
-        calc.pressDigitKey(20);
-        calc.pressBinaryOperationKey("x");
-        calc.pressNegativeKey();
-        calc.pressDigitKey(20);
-        calc.pressEqualsKey();
-        assertEquals("-400", calc.readScreen());
-    }
 
-    @Test
-    @DisplayName("should display the correct number after minusing two numbers, " +
-            "one of them with dot(double or float number)")
-    void calculatorCanUseDotKey() {
-        Calculator calc = new Calculator();
-        calc.pressDigitKey(-5);
-        calc.pressDotKey();
-        calc.pressDigitKey(5);
-        calc.pressBinaryOperationKey("-");
-        calc.pressDigitKey(1);
-        calc.pressEqualsKey();
-        assertEquals("-6.5",calc.readScreen());
-    }
 
 }
 
