462ab5b6fbd0d7ecde1e6c83aa1581f41327038b
floriananthes <s0578399@htw-berlin.de>
2021-04-25 20:41:28 +0200

    Add green test multiplyingNumbers

--- app/src/test/java/prog2/ha1/testing/CalculatorTest.java
+++ app/src/test/java/prog2/ha1/testing/CalculatorTest.java
@@ -37,6 +37,24 @@ class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    @DisplayName("should display result after multiplying two positive numbers")
+    void testPositiveMultiplying() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(2);
+        calc.pressBinaryOperationKey("x");
+        calc.pressDigitKey(2);
+        calc.pressEqualsKey();
+
+        String expected = "4";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+
+
     //TODO hier weitere Tests erstellen
 }
 
