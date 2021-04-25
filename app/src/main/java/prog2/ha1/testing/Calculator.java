41568c946f3809f7b0193814576342088aa750d9
Mustafa.Rashidy <Mustafa.Rashidy@Student.HTW-Berlin.de>
2021-04-25 22:39:16 +0200

    Commit für Fix zu diesem Test.

--- app/src/main/java/prog2/ha1/testing/Calculator.java
+++ app/src/main/java/prog2/ha1/testing/Calculator.java
@@ -18,11 +18,13 @@ public class Calculator {
 
         if(screen.equals("0")) screen = "";
 
-        if(latestOperation.isEmpty()) {
+        if(latestOperation.isEmpty() || screen.endsWith(".")) {
             screen = screen + digit;
+
         } else {
             latestValue = Double.parseDouble(screen);
             screen = Integer.toString(digit);
+
         }
     }
 
