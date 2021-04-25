a997409b016dcd652ff63cd1ca130c3c96659990
s0565958 <s0565958@htw-berlin.de>
2021-04-25 13:22:49 +0200

    1. Grüner Test

--- app/src/main/java/prog2/ha1/testing/Calculator.java
+++ app/src/main/java/prog2/ha1/testing/Calculator.java
@@ -3,7 +3,7 @@ package prog2.ha1.testing;
 // behaviour inspired by https://www.online-calculator.com/
 public class Calculator {
 
-    private String screen = "0";
+    private java.lang.String screen = "0";
 
     private double latestValue;
 
@@ -37,6 +37,12 @@ public class Calculator {
     }
 
     public void pressUnaryOperationKey(String operation) { // also die Tasten Wurzel, %, 1/x
+        var result = switch (operation){
+            case "sqrt" -> Math.sqrt(Double.parseDouble(screen));
+            default -> throw new IllegalArgumentException();
+
+        };
+        screen = Double.toString(result);
 
     }
 
@@ -59,4 +65,5 @@ public class Calculator {
         screen = Double.toString(result);
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
     }
+
 }
