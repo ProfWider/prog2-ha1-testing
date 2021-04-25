9aea6558611df8af432367afea36d2e7ccf3bc59
Samet Dogan <Samet.d421@gmail.com>
2021-04-25 16:23:33 +0200

    Teilaufgabe 2: fixed roter Test (division mit null)

--- app/src/main/java/prog2/ha1/testing/Calculator.java
+++ app/src/main/java/prog2/ha1/testing/Calculator.java
@@ -58,5 +58,7 @@ public class Calculator {
         };
         screen = Double.toString(result);
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
+        if(screen.equals("Infinity")) screen = "Error";
+
     }
 }
